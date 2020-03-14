9788dbc5b60f4933630b7c155af0926d1740f42f
olafironfoot <Erik.gwl@gmail.com>
2020-03-14 19:42:55 +1100

    add!test

--- Lectures/Lecture4/src4/classes3.py
+++ Lectures/Lecture4/src4/classes3.py
@@ -9,9 +9,14 @@ class Flight:
         print(f"Flight origin: {self.origin}")
         print(f"Flight destination: {self.destination}")
         print(f"Flight duration: {self.duration}")
+        print("\n")
 
     def delay(self, amount):
         self.duration += amount
+
+    #self created, print hellow world if used this
+    def print_hello(self):
+        print("hello world only lah, relax")
 
 
 def main():
@@ -19,6 +24,7 @@ def main():
     f1 = Flight(origin="New York", destination="Paris", duration=540)
     f1.delay(10)
     f1.print_info()
+    f1.print_hello()
 
 
 if __name__ == "__main__":
