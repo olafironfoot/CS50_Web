c05c015679e498b566ab96a7c482921312d36cb3
olafironfoot <Erik.gwl@gmail.com>
2020-03-16 08:47:01 +1100

    Update models.py

--- Lectures/Lecture4/src4/models.py
+++ Lectures/Lecture4/src4/models.py
@@ -4,8 +4,8 @@ db = SQLAlchemy()   #Olaf:"creates a database"
 
 class Flight(db.Model): #Olaf:"defines a class, but QN: what does db.Model do"
     __tablename__ = "flights"   #Olaf:"names the table"
-    id = db.Column(db.Integer, primary_key=True)    #Olaf:"creates an object using the SQLAlchemy class/package and giving it attributes, db.Integer, and setting primary_key as True, then linking it to the python variable id"
-    origin = db.Column(db.String, nullable=False)   #Olaf:""
+    id = db.Column(db.Integer, primary_key=True)    #Olaf:"creates an object under the class Flight which requires arguments from "db.Model"(probably a variable in SQL Alchemy that contants argument names). And by using SQLAlchemy package, give it attributes, db.Integer, and primary_key, then linking it to the python variable `id` "
+    origin = db.Column(db.String, nullable=False)   #Olaf:"creates an object(under class Flight), setting db.Column's variables before passing it into `Origin` "
     destination = db.Column(db.String, nullable=False)
     duration = db.Column(db.Integer, nullable=False)
 
@@ -14,4 +14,4 @@ class Passenger(db.Model):
     __tablename__ = "passengers"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    flight_id = db.Column(db.Integer, db.ForeignKey("flights.id"), nullable=False)
+    flight_id = db.Column(db.Integer, db.ForeignKey("flights.id"), nullable=False)  #Olaf:"creates an object with similar attributes passed in except for an addition attribute/proprety db.ForeignKey("flight.id")"
